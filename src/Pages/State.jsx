6da5b8139691cc6b9dbb8843e67c11ba38bc8900
M Zubair <md.zubair143598@gmail.com>
2022-10-10 18:03:27 +0500

    set reset button

--- src/Pages/State.jsx
+++ src/Pages/State.jsx
@@ -6,15 +6,22 @@ function State() {
   function incriment() {
     setCount(prevCount => prevCount+1);
   }
+
   function decriment() {
     setCount(prevCount => prevCount-1);
   }
+
+  function reset()
+{
+  setCount(0)
+}
 
   return (
     <div>
       <button className="btn" onClick={decriment}>-</button>
       <span>{count}</span>
       <button className="btn" onClick={incriment}>+</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
